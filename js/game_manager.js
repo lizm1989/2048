e652c9e864a5b747e61f3e72ee25f3cfc9e3fb91
杨兵强 <yangbingqaing@gmail.com>
2014-06-25 14:51:30 +0800

    a

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,8 +59,8 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var type = Math.random() < 0.8 ? 'number' : 'operator';
-    var value = Math.random() < (type === 'number' ? 0.9 : 0.8) ? 10 : 10;
+    var type = Math.random() < 0.2 ? 'number' : 'operator';
+    var value = Math.random() < (type === 'number' ? 0.9 : 0.8) ? 4 : 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value, type);
 
     this.grid.insertTile(tile);
